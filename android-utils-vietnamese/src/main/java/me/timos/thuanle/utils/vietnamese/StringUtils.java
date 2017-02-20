0c4914800b7631c13908616f4ce46baba978f7c1
ThuanLe <thuanle@hcmut.edu.vn>
2017-02-21 06:37:30 +0700

    Version 1.0.0. Add document.

--- android-utils-vietnamese/src/main/java/me/timos/thuanle/utils/vietnamese/StringUtils.java
+++ android-utils-vietnamese/src/main/java/me/timos/thuanle/utils/vietnamese/StringUtils.java
@@ -3,7 +3,18 @@ package me.timos.thuanle.utils.vietnamese;
 import java.text.Normalizer;
 import java.util.regex.Pattern;
 
+/**
+ * String utilities for Vietnamese
+ */
 public class StringUtils {
+
+    /**
+     * <p>Remove accent of specific string.</p>
+     * <p>For example: The string "Cộng Hòa Xã Hội Chủ Nghĩa Việt Nam - Độc Lập - Tự Do - Hạnh Phúc" will be converted into "Cong Hoa Xa Hoi Chu Nghia Viet Nam - Doc Lap - Tu Do - Hanh Phuc"</p>
+     *
+     * @param s string need to remove accent
+     * @return the accent removed string
+     */
     public static String removeAccent(String s) {
         String temp = Normalizer.normalize(s, Normalizer.Form.NFD);
         Pattern pattern = Pattern.compile("\\p{InCombiningDiacriticalMarks}+");
